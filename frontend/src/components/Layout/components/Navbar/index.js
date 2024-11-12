acef3482067825bbaa5fe7cc51f25352abeb5569
Kevin <hoang.kevin.1110@gmail.com>
2024-11-12 22:22:11 +0700

    update

--- frontend/src/components/Layout/components/Navbar/index.js
+++ frontend/src/components/Layout/components/Navbar/index.js
@@ -137,7 +137,7 @@ function Navbar(props) {
                     </div>
                 </Link>
                 <Link
-                    to="https://t.me/CSKHLOTTEYOUNGPLAZA"
+                    to="https://support.biendich68.com"
                     target="_blank"
                     className="flex-1 flex justify-center text-center"
                 >
